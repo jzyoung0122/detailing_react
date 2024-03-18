046ad4ed2a692b31636e4e6bc406452ace78b3ea
jzyoung0122 <142475319+jzyoung0122@users.noreply.github.com>
2024-03-18 23:23:25 +1100

    Delete src directory

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const http = axios.create({
-  // baseURL: '/api',
-  baseURL: '/',
-});
-
-export default http;
--- src/img/imgs.js
+++
@@ -1,3 +0,0 @@
-export const interiorImg=require('./car-interior-detailing-vip.jpeg')
-
-export const imgContext = require.context('./', true, /\.(jpg|jpeg)$/).keys();
